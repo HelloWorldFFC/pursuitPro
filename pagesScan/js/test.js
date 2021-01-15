ca5abe6d3b940f876a528534568fa718ac80ffac
chengfangfang <chengfangfang@qq.com>
2021-01-16 02:37:34 +0800

    测试

---
+++ pagesScan/js/test.js
@@ -0,0 +1,6 @@
+const ministry_url = {
+	//中华人民共和国文化和旅游部 ministry of culture and tourism of the people's republic of china
+	cult_tour:'https://www.mct.gov.cn/',
+	cult_tour_scenicspot_5A:'https://zwfw.mct.gov.cn/scenicspot',
+	
+	}
